5784b4d6f449c8086c7fcf9979891125f471f551
aarish <aarishalam22@gmail.com>
2026-03-16 23:32:16 +0530

    fix route

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
  * - @access Private
  * - @description This route allows authenticated users to create a new bank account. The request body should include the account type (e.g., "savings" or "checking") and an optional initial deposit amount. The route handler will validate the input, create a new account associated with the authenticated user, and return the account details in the response.
  */
-router.post("/create", authMiddleware.authMiddleware, accountController.createAccountController)
+router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
 
 
 module.exports = router
